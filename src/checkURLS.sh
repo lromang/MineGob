9d1d930efb2e4d6672b6d7bb75b47d831ab79521
lromang <luis.roangarci@gmail.com>
2017-05-16 10:59:55 -0500

    print utf-8

--- src/checkURLS.sh
+++ src/checkURLS.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-for i in $(tail -n +2  ../data/MAT.csv | awk -F ',' '{print $13}')
+for i in $(tail -n +2  ../data/all_urls.csv)
 do
     # Time prior URL verification.
-    T="$(date)"
+    T0=$(date +%s)
 
     # URL verification.
     urlstatus=$(timeout $1s curl -k -o /dev/null --silent --head --write-out '%{http_code}' "$i" )
 
     # Verification time.
-    T=$(date-$T)
+    T1=$(date +%s)
 
     # Check if timeout.
     if [ $(echo $urlstatus | wc -m) -eq 1 ]
@@ -18,8 +18,8 @@ do
     fi
 
     # Print results.
-    echo  "status: $urlstatus | execution time: $T | date: `date`"
+    echo  "status: $urlstatus | execution time: $(($T1 - $T0)) | date: `date`"
 
     # Send results to file.
-    echo -e  "$urlstatus \t $T \t `date`" >> urlstatus.csv
+    echo -e  "$urlstatus \t $T0 \t `date`" >> urlstatus.csv
 done
--- src/process.R
+++ src/process.R
@@ -29,7 +29,7 @@ suppressPackageStartupMessages(library(ggplot2))
 
 ## Read in dataset
 mat    <- read.csv("../data/MAT.csv",
-                  stringsAsFactors = FALSE)
+                    stringsAsFactors = FALSE)
 
 ## Get institutions with inventory and plan
 invent <- mat[str_detect(mat$conj_tit,
@@ -66,7 +66,7 @@ dates <- mat.dc[, max(fecha_m), by = "slug"]
 ## Put it all together
 ###################################
 entities <- data.frame(apply(
-    mat[, c(11, 4)],
+    select(mat, c(dep, slug)),
     2,
     unique))
 entities$tiene_inv  <- entities$slug %in% invent$slug
@@ -126,4 +126,212 @@ names(new_data) <- names(final_data)
 final_data      <- rbind(final_data, new_data)
 write.csv(final_data,
           "../data/dataset_presidencia.csv",
+          row.names = FALSE)
+
+
+###################################
+## Google Analytics
+###################################
+## Profiles
+##        id              name
+## 1 88225771 All Web Site Data
+## 2 91319629 All Web Site Data DESCARGAS 2014/10/01 - today
+## 3 91863615 All Web Site Data VISITAS   2014/10/01 - today
+
+## token <- Auth("497323299158-elgh4c5t1o57dd8qfvakr99ge6d2qge3.apps.googleusercontent.com",
+##              "I7wDgHvwULRHkB7bjt_JrrJt")
+## save(token, file="oauth_token")
+## load("oauth_token")
+## Validate Token
+## ValidateToken(token)
+authorize()
+## -----------------------------
+## Queries
+##
+## Downloads :
+## 85102504(2014-07-01 -> 2014-09-30)
+## +
+## 91319629(2014-10-01 -> today)
+##
+## Sessions:
+## 91863615(2014-07-01 -> today)
+## +
+## 91319629(2014-10-01 -> today)
+## -----------------------------
+yesterday <- today()
+
+##################################################
+## Downloads
+##################################################
+
+## (2014-10-01 -> today)
+## CKAN
+id <- 91319629
+query.downloads.ckan <- get_ga(id, start.date   = "2014-07-01",
+                              end.date    = yesterday,
+                              dimensions  = "ga:date, ga:pagePath, ga:hour",
+                              metrics     = "ga:totalEvents",
+                              sort        = "-ga:date",
+                              fetch.by    = "day",
+                              max.results = NULL)
+analytic_downloads <- sum(query.downloads.ckan$totalEvents)
+## all.queries.downloads <- query.downloads
+
+## Date
+query.downloads.ckan$date        <- as.Date(query.downloads.ckan$date)
+query.downloads.ckan$date_n_time <- strptime(paste0(query.downloads.ckan$date, ":",
+                                              query.downloads.ckan$hour),
+                                       format="%Y-%m-%d:%H")
+## (2014-07-01 -> 2014-09-30)
+## BETA
+id <- 88225771
+query.downloads.beta <- get_ga(id, start.date   = "2014-07-01",
+                              end.date    = "2014-09-30",
+                              dimensions  = "ga:date, ga:pagePath, ga:hour",
+                              metrics     = "ga:totalEvents",
+                              sort        = "-ga:date",
+                              fetch.by    = "day",
+                              max.results = NULL)
+## all downloads
+analytic_downloads <- analytic_downloads + sum(query.downloads.beta$totalEvents)
+
+## Date
+query.downloads.beta$date        <- as.Date(query.downloads.beta$date)
+query.downloads.beta$date_n_time <- strptime(paste0(query.downloads.beta$date, ":",
+                                              query.downloads.beta$hour),
+                                            format="%Y-%m-%d:%H")
+
+
+## ------------------------------
+## ALL DOWNLOADS
+## ------------------------------
+all.downloads <- rbind(query.downloads.ckan,
+                      query.downloads.beta)
+all.downloads <- all.downloads[all.downloads$totalEvents > 0,]
+
+## Conj Downloads
+all.downloads$conj.downloads <- str_extract(all.downloads$pagePath,
+                                           "/dataset/([[:alnum:]]+-?)+") %>%
+    str_replace("/dataset/","")
+
+## Rec Downloads
+all.downloads$rec.downloads <- str_extract(all.downloads$pagePath,
+                                          "/resource/([[:alnum:]]+-?)+") %>%
+    str_replace("/resource/","")
+## ------------------------------
+## data-table
+## ------------------------------
+all.downloads.t <- data.table(all.downloads)
+
+## CONJ
+conj.join.mat <- select(mat, c(dep, conj_tit, conj, conj_desc))
+conj.join.mat <- filter(conj.join.mat, !duplicated(conj.join.mat))
+
+all.downloads.t.conj <- all.downloads.t[, sum(totalEvents),
+                                       by = c("date", "conj.downloads")]
+names(all.downloads.t.conj) <- c("date", "conj", "downloads")
+all.downloads.t.conj        <- na.omit(all.downloads.t.conj)
+all.downloads.conj          <- merge(all.downloads.t.conj,
+                                    conj.join.mat,
+                                    by = "conj",
+                                    all.x = TRUE) %>%
+    na.omit()
+
+## Month year
+all.downloads.conj$month <- month(all.downloads.conj$date)
+all.downloads.conj$year  <- year(all.downloads.conj$date)
+write.csv(all.downloads.conj, "../data/downloads_conj.csv", row.names = FALSE)
+
+## Resumen Conj
+conj.summary        <- all.downloads.conj[, sum(downloads), by = c("dep", "conj", "conj_desc")]
+names(conj.summary) <- c("dep", "conj", "conj_desc", "downloads")
+write.csv(conj.summary, "../data/summary_conj.csv", row.names = FALSE)
+
+## REC
+rec.join.mat <- select(mat, c(dep, rec_id, rec, rec_des))
+rec.join.mat <- filter(rec.join.mat,
+                      !duplicated(rec.join.mat))
+
+all.downloads.t.rec  <- all.downloads.t[, sum(totalEvents),
+                                      by = c("date", "rec.downloads")]
+names(all.downloads.t.rec) <- c("date", "rec_id", "downloads")
+all.downloads.t.rec        <- na.omit(all.downloads.t.rec)
+all.downloads.rec          <- merge(all.downloads.t.rec,
+                                    rec.join.mat,
+                                    by = "rec_id",
+                                    all.x = TRUE) %>%
+    na.omit()
+
+## Month year
+all.downloads.rec$month <- month(all.downloads.rec$date)
+all.downloads.rec$year  <- year(all.downloads.rec$date)
+
+write.csv(all.downloads.rec, "../data/downloads_rec.csv", row.names = FALSE)
+
+## Resumen Rec
+rec.summary        <- all.downloads.rec[, sum(downloads), by = c("dep", "rec", "rec_des")]
+names(rec.summary) <- c("dep", "rec", "rec_desc", "downloads")
+write.csv(rec.summary, "../data/summary_rec.csv", row.names = FALSE)
+
+
+##################################################
+## Visites
+##################################################
+
+id <- 91863615
+query.visits <- get_ga(id, start.date   = "2014-10-01",
+                      end.date    = yesterday,
+                      dimensions  = "ga:date, ga:pagePath, ga:hour, ga:medium",
+                      metrics     = "ga:sessions,ga:pageviews,ga:bounces",
+                      sort        = "-ga:date",
+                      fetch.by    = "day",
+                      max.results = NULL)
+analytic_visits  <- sum(query.visits$sessions)
+analytic_bounces <- sum(query.visits$bounces)
+## Date
+query.visits$date <- as.Date(query.visits$date)
+query.visits$date_n_time <- strptime(paste0(query.visits$date, ":", query.visits$hour),
+                                       format="%Y-%m-%d:%H")
+png("visitas.png")
+ggplot(data = query.visits, aes(x = date_n_time, y = sessions)) +
+    geom_point( color = "#00cc99") + geom_jitter(color = "#00cc99") +
+theme(panel.background = element_blank(), axis.title = element_text(colour = "#424242")) +
+ylab("Número de visitas") + xlab("Fecha")
+dev.off()
+
+## -----------------------------
+## Daily data
+## -----------------------------
+## Daily downloads
+q.downloads <- data.table(query.downloads)
+down_dates  <- q.downloads[,sum(totalEvents), by = "date"]
+names(down_dates) <- c("fecha",
+                      "Descargas")
+## Daily visits
+q.visits <- data.table(query.visits)
+vis_dates  <- q.visits[,sum(sessions), by = "date"]
+names(vis_dates) <- c("fecha",
+                      "Visitas")
+write.csv(vis_dates,
+          "data_visits.csv",
+          row.names = FALSE)
+
+## -------------------
+## Visites CKAN
+## -------------------
+id <- 91319629
+query.visits.ckan <- get_ga(id, start.date   = "2014-07-01",
+                      end.date    = yesterday,
+                      dimensions  = "ga:date, ga:pagePath, ga:hour",
+                      metrics     = "ga:sessions",
+                      sort        = "-ga:date",
+                      fetch.by    = "day",
+                      max.results = NULL)
+analytic_visits.ckan  <- sum(query.visits.ckan$sessions)
+
+##################################################
+## SAVE URLS
+##################################################
+write.csv("../data/all_urls.csv",
+          mat$rec_url,
           row.names = FALSE)
